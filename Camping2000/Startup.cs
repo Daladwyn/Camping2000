db121f51747de48cb32463b3634459b5e04efd50
Christian Levin <christian.johansson@gmail.com>
2018-06-07 10:27:20 +0200

    Add project files.

---
+++ Camping2000/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Camping2000.Startup))]
+namespace Camping2000
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
