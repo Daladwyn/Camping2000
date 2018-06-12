f8f69ed82750a70e1e2c3121f5863e94f3eb6540
Christian Levin <christian.johansson@gmail.com>
2018-06-12 16:30:34 +0200

    all views except change user details is complete

--- Camping2000/Controllers/HomeController.cs
+++ Camping2000/Controllers/HomeController.cs
@@ -16,32 +16,92 @@ namespace Camping2000.Controllers
         public ActionResult SpaceForTent()
         {
             ViewBag.Message = "Renting space for tent.";
-
             return PartialView("_SpaceForTent");
         }
-
         public ActionResult SpaceForCaravan()
         {
             ViewBag.Message = "Renting space for caravan.";
-
             return PartialView("_SpaceForCaravan");
-        }
-        public ActionResult ConfirmSpaceForTent()
-        {
-            return PartialView("_ConfirmSpaceForTent");
-        }
-        public ActionResult ConfirmSpaceForCaravan()
-        {
-            return PartialView("_ConfirmSpaceForCaravan");
-                    
         }
         public ActionResult RentSpaceForTent()
         {
-            return PartialView("_ReservedConfirmation");
+            return PartialView("_ConfirmSpaceForTent");
         }
         public ActionResult RentSpaceForCaravan()
         {
+            return PartialView("_ConfirmSpaceForCaravan");
+        }
+        public ActionResult ConfirmSpaceForTent()
+        {
             return PartialView("_ReservedConfirmation");
+        }
+        public ActionResult ConfirmSpaceForCaravan()
+        {
+            return PartialView("_ReservedConfirmation");
+        }
+        public ActionResult PrintReservation()
+        {
+            return PartialView("Index");
+        }
+        public ActionResult CheckIn()
+        {
+            return PartialView("_CheckIn");
+        }
+        public ActionResult CheckOut()
+        {
+            return PartialView("_CheckOut");
+        }
+        public ActionResult ArrivalsDepartures()
+        {
+            return PartialView("_ArrivalsDepartures");
+        }
+        public ActionResult CheckInConfirmation()
+        {
+            return PartialView("_CheckInConfirmation");
+        }
+        public ActionResult CheckOutConfirmation()
+        {
+            return PartialView("_CheckOutConfirmation");
+        }
+        public ActionResult GoToStart()
+        {
+            return PartialView("Index");
+        }
+        public ActionResult ShowGuestArrivals()
+        {
+            return PartialView("_ShowGuestArrivals");
+        }
+        public ActionResult ShowGuestDepartures() 
+            {
+            return PartialView("_ShowGuestDepartures");
+            }
+        public ActionResult ModifyBooking()
+        {
+            return PartialView("_ModifyBooking");
+        }
+        public ActionResult ModifyGuestDetails()
+        {
+            return PartialView("_ModifyGuestDetails");
+        }
+        public ActionResult ListPresentBookings()
+        {
+            return PartialView("_PresentBooking");
+        }
+        public ActionResult ModifySpecificBooking()
+        {
+            return PartialView("_ModifySpecificBooking");
+        }
+        public ActionResult UpdatedBooking()
+        {
+            return PartialView("_UpdatedBooking");
+        }
+        public ActionResult SearchForGuest()
+        {
+            return PartialView("_ShowFoundGuests");
+        }
+        public ActionResult ModifySpecificGuestDetails()
+        {
+            return PartialView("_GuestDetails");
         }
     }
 }
