de616dd817a162fc690858e539075493d7b8325d
Christian Levin <christian.johansson@gmail.com>
2018-09-20 11:09:53 +0200

    Copy adress button is complete

--- Camping2000/Scripts/main.js
+++ Camping2000/Scripts/main.js
@@ -2,13 +2,13 @@
 
     var elementInDataNames = ["Js-PostAdressStreet1", "Js-PostAdressStreet2", "Js-PostAdressStreet3", "Js-PostAdressZipCode", "Js-PostAdressCity"];
     var elementOutDataNames = ["Js-LivingAdressStreet1", "Js-LivingAdressStreet2", "Js-LivingAdressStreet3", "Js-LivingAdressZipCode", "Js-LivingAdressCity"];
-    var elementParents = ["LAS1", "LAS2", "LAS3", "LAZC", "LAC"]
+    var elementParents = ["LAS1", "LAS2", "LAS3", "LAZC", "LAC"];
     var elementValues = [];
     var elementsForUser = ["Your Postadress line 1", "Your Postadress line 2", "Your Postadress line 3", "Your Postzipcode", "Your Posttown"];
     var checkedmessage = "";
     printErrorMessage("");
     for (var i = 0; i <= 4; i++) {
-        elementValues.push(document.getElementById(elementInDataNames[i]).value)
+        elementValues.push(document.getElementById(elementInDataNames[i]).value);
     }
     var validData = validateData(elementValues);
     checkedmessage = errorMessage(validData, elementsForUser);
@@ -36,19 +36,22 @@ function validateData(values) {
     var validData = [];
     if (isArray(values)) {
         for (var i = 0; i <= 4; i++) {
-            if ((i !== 3) && (values[i].length >= 100)) {
+            if (i !== 3 && values[i].length >= 100) {
                 validData.push("Too many letters/numbers is given");
-            } else if ((i !== 3) && (values[i].length === 0)) {
+            } else if (i !== 3 && values[i].length === 0) {
                 validData.push("Too few letters/numbers is given");
-            } else if ((i === 3) && (values[i].length < 5) || (values[i].length > 5)) {
-                validData.push("Incorrect zipcode");
+            } else if (i === 3 && isNaN(values[3])) {
+                validData.push("Your must type numbers");
+            } else if (i === 3 && (values[3].length < 5 || values[3].length > 5)) {
+                validData.push("You have too few or too many numbers");
             } else {
                 validData.push("Correct Data.");
             }
         }
     } else {
-        validData.push("No values was read")
+        validData.push("No values was read");
     }
+    console.log("ValidData is: " + validData);
     return validData;
 }
 
@@ -64,12 +67,9 @@ function printErrorMessage(errorMessage) {
 function errorMessage(validData, elementsForUser) {
     var errorProperties = "";
     for (var i = 0; i <= 4; i++) {
-        if (validData[i] === "Too many letters/numbers is given") {
+        if (validData[i] !== "Correct Data.") {
             errorProperties = errorProperties + validData[i] + " in " + elementsForUser[i] + ". \n";
-        } else if (validData[i] === "Too few letters/numbers is given") {
-            errorProperties = errorProperties + validData[i] + " in " + elementsForUser[i] + ". \n";
-        } else if (validData[i] === "Incorrect zipcode") {
-            errorProperties = errorProperties + validData[i] + " in " + elementsForUser[i] + ". \n";
+            console.log(errorProperties);
         } else if (validData[i] === "No values was read") {
             errorProperties = errorProperties + validData[i] + " from the form, please try again. \n";
         }
